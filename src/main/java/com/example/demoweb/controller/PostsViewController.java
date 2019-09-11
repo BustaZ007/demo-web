24cf63ce018855d4f3decbe5202105a46ceb80ab
Pavel <pavel.zaborin@inbox.ru>
2019-09-11 23:35:00 +0500

    Controller commit

--- src/main/java/com/example/demoweb/controller/PostsViewController.java
+++ src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -1,4 +1,24 @@
 package com.example.demoweb.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class PostsViewController {
+
+    @ResponseBody
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String list() {
+        return "Здесь будет главная страница";
+    }
+
+    @ResponseBody
+    @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
+    public String single(@PathVariable("id") Long id) {
+        return "Здесь будет страница поста №" + id;
+    }
+
 }
