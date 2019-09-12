1e680aa177b6500e0e598a2fa0c60de320c0d759
Pavel <pavel.zaborin@inbox.ru>
2019-09-12 10:06:51 +0500

    Posts and Likes 2.0

--- src/main/java/com/example/demoweb/controller/PostsViewController.java
+++ src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -1,21 +1,33 @@
 package com.example.demoweb.controller;
 
+import com.example.demoweb.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
+
+import java.util.Map;
 
 @Controller
 public class PostsViewController {
 
-    @ResponseBody
-    @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String list() {
-        return "Здесь будет главная страница";
+    @Autowired
+    PostService postsService;
+
+
+  //  @ResponseBody
+    @RequestMapping(path ="/", method = RequestMethod.GET)
+    public String list(Model model) {
+        model.addAttribute("appName", "Моё супер приложение");
+        model.addAttribute("posts", postsService.listAllPosts());
+        return "list";
     }
 
-    @ResponseBody
+
+ //   @ResponseBody
     @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
     public String single(@PathVariable("id") Long id) {
         return "Здесь будет страница поста №" + id;
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -1,4 +1,15 @@
 package com.example.demoweb.service;
 
+import com.example.demoweb.model.Post;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PostService {
+
+    public Post[] listAllPosts(){
+        return new Post[] {new Post("Привет"), new Post("мир"), new Post("!"),
+        };
+    }
 }
