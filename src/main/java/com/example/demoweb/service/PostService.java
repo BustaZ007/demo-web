db166995beb0c241228ed3ac2c21f157ba89c761
Pavel <pavel.zaborin@inbox.ru>
2019-09-12 11:33:01 +0500

    Posts publication

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -3,16 +3,27 @@ package com.example.demoweb.service;
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
 
-    public Post[] listAllPosts(){
-        return new Post[] {new Post("Привет", new Date()),
-                new Post("мир", new Date()),
-                new Post("!", new Date()),
-        };
+    private List<Post> posts = new ArrayList<>();
+
+    {
+        posts.add(new Post("Привет", new Date()));
+        posts.add(new Post("мир", new Date()));
+        posts.add(new Post("!", new Date()));
     }
+
+    public List<Post> listAllPosts(){
+        return posts;
+    }
+
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
+    }
+
 }
