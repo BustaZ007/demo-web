aeba8a31c43071e18b10962d3b05cb0ff34d28d9
Pavel <pavel.zaborin@inbox.ru>
2019-09-12 10:24:47 +0500

    Bootstrap

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -3,13 +3,16 @@ package com.example.demoweb.service;
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
 
     public Post[] listAllPosts(){
-        return new Post[] {new Post("Привет"), new Post("мир"), new Post("!"),
+        return new Post[] {new Post("Привет", new Date()),
+                new Post("мир", new Date()),
+                new Post("!", new Date()),
         };
     }
 }
